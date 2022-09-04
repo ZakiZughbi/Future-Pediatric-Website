c5a6ca1dbdfbad14b046d1510228d5d18aeb25b0
ZakiZughbi <zakizughbi@gmail.com>
2022-09-04 10:32:39 -0400

    load.js error2

--- JS/load.js
+++ JS/load.js
@@ -4,8 +4,8 @@
 window.addEventListener('load', function() {
     const script1 = document.createElement('script');
     const script2 = document.createElement('script');
-    script.src = 'app.js';
-    script.src = 'swiper-bundle.min.js';
+    script1.src = 'app.js';
+    script2.src = 'swiper-bundle.min.js';
     document.body.appendChild(script1);
     document.body.appendChild(script2);
 });
