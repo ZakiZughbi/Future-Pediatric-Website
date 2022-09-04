30986302d5138696f54b0cfb36410958de7ce3e2
ZakiZughbi <zakizughbi@gmail.com>
2022-09-04 10:22:40 -0400

    opt js, load.js

---
+++ JS/load.js
@@ -0,0 +1,1 @@
+window.addEventListener("load",function(){let a=document.createElement("script"),b=document.createElement("script");script.src="app.js",script.src="swiper-bundle.min.js",document.body.appendChild(a),document.body.appendChild(b)})
