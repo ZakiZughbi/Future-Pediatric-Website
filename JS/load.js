1afac810f5753ac5772ca1ae59776a987081218f
ZakiZughbi <zakizughbi@gmail.com>
2022-09-04 10:32:02 -0400

    load.js error

--- JS/load.js
+++ JS/load.js
@@ -1,1 +1,11 @@
-window.addEventListener("load",function(){let a=document.createElement("script"),b=document.createElement("script");script.src="app.js",script.src="swiper-bundle.min.js",document.body.appendChild(a),document.body.appendChild(b)})
+// window.addEventListener("load",function(){let a=document.createElement("script"),b=document.createElement("script");script.src="app.js",script.src="swiper-bundle.min.js",document.body.appendChild(a),document.body.appendChild(b)})
+
+
+window.addEventListener('load', function() {
+    const script1 = document.createElement('script');
+    const script2 = document.createElement('script');
+    script.src = 'app.js';
+    script.src = 'swiper-bundle.min.js';
+    document.body.appendChild(script1);
+    document.body.appendChild(script2);
+});
